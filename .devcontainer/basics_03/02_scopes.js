5f63d642829a922aa148e934b3c6b1af28119ef2
Himm04 <flashhimanshu27@gmail.com>
2024-08-06 09:22:03 +0000

    Scopes

---
+++ .devcontainer/basics_03/02_scopes.js
@@ -0,0 +1,50 @@
+let a = 80
+if(true){
+
+    let a = 10 
+    const b = 20
+    var c = 40 
+// console.log(a);
+}
+
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+function one(){
+
+const username = "Himanshu"
+
+function Two(){
+
+    const website = "youtube"
+    console.log(username);
+
+}
+
+ Two()
+
+} 
+// one() 
+
+if(true){
+    const username = "Himanshu"
+    if(username === "Himanshu"){
+        const website = " youtube"
+        // console.log(username + website); 
+    }
+} 
+
+// ++++++++++++++++++ Instrusting ++++++++++++++++++++ 
+
+addone(7)  // <=
+
+function addone (num){
+    return num + 1 
+} 
+
+const addTwo = function(num){
+    return num + 2 
+} 
+
+addTwo(6)  // <=
